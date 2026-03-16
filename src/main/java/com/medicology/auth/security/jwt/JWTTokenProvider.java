25732f458e3432a1d0e4bf747f4e003c4098509f
KhoaLND <lengocdangkhoak16@siu.edu.vn>
2026-03-17 01:19:01 +0700

    feat: add JWT services + email services

---
+++ src/main/java/com/medicology/auth/security/jwt/JWTTokenProvider.java
@@ -0,0 +1,59 @@
+package com.medicology.auth.security.jwt;
+
+import com.medicology.auth.entity.RefreshToken;
+import com.medicology.auth.entity.User;
+import com.medicology.auth.repository.RefreshTokenRepository;
+
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.Jwts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JWTTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${access.token.expiration}")
+    private long accessTokenExpiration;
+
+    @Value("${refresh.token.expiration}")
+    private long refreshTokenExpiration;
+
+    @Autowired private RefreshTokenRepository refreshTokenRepository;
+
+    SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    
+    public String generateAccessToken(User user) {
+        return generateToken(user, accessTokenExpiration, "access");
+    }
+    public String generateRefreshToken(User user) {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(user);
+        refreshToken.setExpiresAt(LocalDateTime.now().plusSeconds(refreshTokenExpiration / 1000));
+        refreshToken.setToken(generateToken(user, refreshTokenExpiration, "refresh"));
+        refreshTokenRepository.save(refreshToken);
+        return refreshToken.getToken();
+    }
+
+    // 1. Hàm tạo Token
+    private String generateToken(User user, long expiration,String type) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .claim("type", type)
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+
+}
---
+++ src/main/java/com/medicology/auth/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.medicology.auth.service;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    private org.thymeleaf.TemplateEngine templateEngine;
+    public void sendVerificationEmail(String email, UUID token,String type) {
+        org.thymeleaf.context.Context context = new org.thymeleaf.context.Context();
+
+        context.setVariable("verifyUrl", "http://localhost:8080/api/auth/" + type + "?token=" + token);
+        context.setVariable("deleteUrl", "http://localhost:8080/api/auth/delete?token=" + token);
+        
+        String content = templateEngine.process("EmailTemplate", context);
+        MimeMessage message = mailSender.createMimeMessage();
+
+        // BẮT ĐẦU KHỐI TRY ĐỂ XỬ LÝ LỖI
+    try {
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        
+        helper.setTo(email);
+        helper.setSubject("Xác thực tài khoản của bạn");
+        helper.setText(content, true);
+        
+        mailSender.send(message); // Đừng quên dòng này để thực sự gửi mail đi nhé!
+        
+    } catch (jakarta.mail.MessagingException e) {
+        // Nếu có lỗi (sai email, server mail sập...) nó sẽ nhảy vào đây
+        throw new RuntimeException("Gửi mail thất bại: " + e.getMessage());
+    }
+    }
+}
